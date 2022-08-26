f898349aeffa1d68ede6a7d57b37a89e74b4ef79
Marsunpaisti <marsunpaisti@gmail.com>
2022-08-26 13:08:36 +0300

    add max31855 test script

--- espresso.py
+++ espresso.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+import time
+import board
+import digitalio
+import adafruit_max31855
+
+spi = board.SPI()
+cs = digitalio.DigitalInOut(board.D8)
+
+max31855 = adafruit_max31855.MAX31855(spi, cs)
+while True:
+    tempC = max31855.temperature
+    tempF = tempC * 9 / 5 + 32
+    print("Temperature: {} C {} F ".format(tempC, tempF))
+    time.sleep(2.0)
