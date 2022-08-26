f36a487df8e91815b70f96623eac63a17a486cc8
Marsunpaisti <marsunpaisti@gmail.com>
2022-08-26 18:30:08 +0300

    Refactoring

--- espresso.py
+++ espresso.py
@@ -4,23 +4,41 @@ import digitalio
 import pwmio
 import adafruit_max31855
 import numpy as np
+import warnings
 
 spi = board.SPI()
 cs = digitalio.DigitalInOut(board.D8)
+max31855 = adafruit_max31855.MAX31855(spi, cs)
 heaterPin = pwmio.PWMOut(board.D4, frequency=1, duty_cycle=0, variable_frequency=False)
 #heaterPin = digitalio.DigitalInOut(board.D4)
 #heaterPin.switch_to_output(False)
 
-max31855 = adafruit_max31855.MAX31855(spi, cs)
-i = 0
-while True:
-    i += 1
-    tempC = max31855.temperature
-    print(f"Temperature: {tempC:.2f}")
-    #state = ("On" if heaterPin.value == True else "Off")
-    #state = ("On" if heaterPin.duty_cycle != 0 else "Off")
-    dutyCycleRatio = 0.5
-    heaterPin.duty_cycle = round(dutyCycleRatio * 65535)
-    #print(f"Pin {heaterPin._pin} {state}")
-        
-    time.sleep(3.0)
+def setHeaterDutyCycle(dutyCycleFraction: float):
+    """
+    Sets heater pin PWM duty cycle from dutyCycleFraction between (0-1), mapping it to (0 - 65535) accordingly
+    """
+    if (dutyCycleFraction > 1):
+        warnings.warn(f"setHeaterDutyCycle dutyCycleFraction should be between 0 and 1, value was {dutyCycleFraction}. Clamped to 1.")
+        dutyCycleFraction = 1
+    if (dutyCycleFraction < 0):
+        warnings.warn(f"setHeaterDutyCycle dutyCycleFraction should be between 0 and 1, value was {dutyCycleFraction}. Clamped to 0.")
+        dutyCycleFraction = 0
+    heaterPin.duty_cycle = round(dutyCycleFraction * 65535)
+
+def readTemperature():
+    """
+    Returns the MAX31855K temperature in celcius.
+    """
+    return max31855.temperature
+
+def main():
+    i = 0
+    while True:
+        i += 1
+        boilerTemperature = readTemperature()
+        print(f"Temperature: {boilerTemperature:.2f}")
+        setHeaterDutyCycle("asd")
+        time.sleep(3.0)
+
+if __name__ == "__main__":
+    main()
