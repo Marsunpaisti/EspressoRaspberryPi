8dedde0d8075c8d4acad64773eebb26bf389e974
Marsunpaisti <marsunpaisti@gmail.com>
2022-09-08 19:39:57 +0300

    Add standalone control module

---
+++ espressoStandalone.py
@@ -0,0 +1,173 @@
+import struct
+import time
+import board
+import digitalio
+import pwmio
+import adafruit_max31855
+import warnings
+import socket
+import argparse
+import os
+import simulinkpid
+
+parser = argparse.ArgumentParser(description="PID Control for Gaggia Classic Pro",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-i", "--ip", action="store",
+                    help="Send data to ip address", required=False, default=None)
+parser.add_argument("-p", "--port", action="store",
+                    help="Send data to port", default=7788)
+parser.add_argument("-d", "--disableprints", action="store_true",
+                    help="Disable prints", default=False),
+args = parser.parse_args()
+config = vars(args)
+DATA_SEND_IP = config["ip"]
+DATA_SEND_PORT = config["port"]
+DISABLE_PRINTS = config["disableprints"]
+SAMPLING_INTERVAL = 0.5
+
+spi = board.SPI()
+cs = digitalio.DigitalInOut(board.D8)
+max31855 = adafruit_max31855.MAX31855(spi, cs)
+
+steamSwitchPin = digitalio.DigitalInOut(board.D23)
+steamSwitchPin.switch_to_input(pull=digitalio.Pull.UP)
+brewSwitchPin = digitalio.DigitalInOut(board.D12)
+brewSwitchPin.switch_to_input(pull=digitalio.Pull.UP)
+pumpPin = digitalio.DigitalInOut(board.D26)
+pumpPin.switch_to_output(False)
+heaterPin = pwmio.PWMOut(board.D4, frequency=2,
+                         duty_cycle=0, variable_frequency=False)
+
+
+latestCommandTimestamp = time.time()
+latestTimeoutTimestamp = time.time()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+lastControlTimestamp = 0
+startedTime = time.time()
+pidController = simulinkpid.DiscretePid(0.046, 0.0018, -0.0030, 3.168544, 1, 0)
+i = 0
+setpoint = 93
+
+
+def setHeaterDutyCycle(dutyCycleFraction: float):
+    """
+    Sets heater pin PWM duty cycle from dutyCycleFraction between (0-1), mapping it to (0 - 65535) accordingly
+    """
+    if (dutyCycleFraction > 1):
+        warnings.warn(
+            f"setHeaterDutyCycle dutyCycleFraction should be between 0 and 1, value was {dutyCycleFraction}. Clamped to 1.")
+        dutyCycleFraction = 1
+    if (dutyCycleFraction < 0):
+        warnings.warn(
+            f"setHeaterDutyCycle dutyCycleFraction should be between 0 and 1, value was {dutyCycleFraction}. Clamped to 0.")
+        dutyCycleFraction = 0
+    heaterPin.duty_cycle = round(dutyCycleFraction * 65535)
+
+
+def togglePump(enabled: bool):
+    pumpPin.value = enabled
+
+
+consecutiveReadTempFails = 0
+latestValidTemp = 20
+
+
+def readTemperature():
+    """
+    Returns the MAX31855K temperature in celcius.
+    """
+    global latestValidTemp
+    global consecutiveReadTempFails
+
+    try:
+        latestValidTemp = max31855.temperature
+        consecutiveReadTempFails = 0
+        return latestValidTemp
+    except RuntimeError as e:
+        consecutiveReadTempFails = consecutiveReadTempFails + 1
+        print(
+            f"Error during readTemperature {e}. Returning latest valid temperature: {latestValidTemp}")
+        if (consecutiveReadTempFails > 10):
+            setHeaterDutyCycle(0)
+            raise RuntimeError(
+                "Too many consecutive temperature read failures.", e)
+        return latestValidTemp
+
+
+def sendToUdp(temperature: float, steamingSwitchState: int, brewSwitchState: int, msgIndex: int):
+    global latestCommandTimestamp
+    global latestTimeoutTimestamp
+    global sock
+    bytes = struct.pack("<ifbb", int(msgIndex), temperature, int(
+        steamingSwitchState), int(brewSwitchState))
+    if (sock != None and DATA_SEND_IP != None):
+        sock.sendto(bytes, (DATA_SEND_IP, DATA_SEND_PORT))
+        if (not DISABLE_PRINTS):
+            print(f"Sent data over UDP. Len: {len(bytes)}: {bytes}")
+
+
+def controlLoop():
+    global latestCommandTimestamp
+    global latestTimeoutTimestamp
+    global lastControlTimestamp
+    global sock
+    global i
+
+    # Always feeding brew switch state to pump
+    brewSwitch = not brewSwitchPin.value
+    togglePump(brewSwitch)
+
+    if (time.time() - lastControlTimestamp < SAMPLING_INTERVAL):
+        return
+    i += 1
+
+    # Send measurements over UDP
+    elapsedTime = time.time() - startedTime
+    boilerTemperature = readTemperature()
+    heaterDutyCycle = heaterPin.duty_cycle / 65535
+    steamingSwitch = not steamSwitchPin.value
+
+    if (not DISABLE_PRINTS):
+        print(f"T: {elapsedTime:.1f} Temp: {boilerTemperature:.1f} HeaterDutyCycle: {heaterDutyCycle:.2f} Steaming: {steamingSwitch:.0f}")
+    sendToUdp(boilerTemperature, steamingSwitch, brewSwitch, i)
+
+    # PID Control
+    pidOutput = pidController.step(setpoint - boilerTemperature, elapsedTime)
+
+    # Brew switch feedforward compensator
+    compensatorOutput = 0.0
+    # Sanity check for cases where brew switch might be intentionally activated to reduce temperature
+    if (brewSwitch and not steamingSwitch and boilerTemperature < (setpoint + 6)):
+        compensatorOutput = 0.14
+
+    output = pidOutput + compensatorOutput
+    # Clamp output
+    if (output > 1):
+        output = 1
+    if (output < 0):
+        output = 0
+
+    setHeaterDutyCycle(output)
+
+    lastControlTimestamp = time.time()
+
+
+def main():
+    global sock
+    print(f"Starting EspressoPi")
+    if (DATA_SEND_IP):
+        print(f"UDP Data send address set to {(DATA_SEND_IP,DATA_SEND_PORT)}")
+
+    while True:
+        controlLoop()
+
+        try:
+            time.sleep(0.01)
+        except KeyboardInterrupt:
+            print("Exiting")
+            sock.close()
+            os._exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ simulinkpid.py
@@ -0,0 +1,72 @@
+from ctypes import *
+from tkinter.messagebox import NO
+from tokenize import Double
+libc = CDLL("./CodegenPid/DiscredePid.so")
+
+# Rename main functions for readability
+cPID_Initialize = libc.PIDController_initialize
+cPID_Step = libc.PIDController_step
+
+# Create both ctypes structures of both state variables
+
+
+class DW_PIDController_T(Structure):
+    _fields_ = [("Integrator_DSTATE", c_double),
+                ("Filter_DSTATE", c_double),
+                ("Integrator_IC_LOADING", c_uint8),
+                ("Filter_IC_LOADING", c_uint8)]
+
+
+class RT_MODEL_PIDController_T(Structure):
+    _fields_ = [("errorStatus", c_char),
+                ("dwork", POINTER(DW_PIDController_T))]
+
+
+class DiscretePid():
+    def __init__(self, pGain: float, iGain: float, dGain: float, filterCoeff: float, upperLimit: float, lowerLimit: float) -> None:
+        self.DW_PIDController_T = DW_PIDController_T()
+        self.RT_MODEL_PIDController_T = RT_MODEL_PIDController_T()
+        self.RT_MODEL_PIDController_T.dwork = pointer(self.DW_PIDController_T)
+        self.ptr_RT_MODEL_PIDController_T = pointer(
+            self.RT_MODEL_PIDController_T)
+        self.error = c_double()
+        self.pGain = c_double()
+        self.iGain = c_double()
+        self.dGain = c_double()
+        self.filterCoeff = c_double()
+        self.integratorState = c_double()
+        self.filterState = c_double()
+        self.upperLimit = c_double()
+        self.lowerLimit = c_double()
+        self.sampleTime = c_double()
+        self.output = c_double()
+        self.ptr_output = pointer(self.output)
+
+        cPID_Initialize(self.ptr_RT_MODEL_PIDController_T, byref(self.error), byref(self.pGain), byref(self.iGain), byref(self.dGain), byref(self.filterCoeff), byref(
+            self.integratorState), byref(self.filterState), byref(self.upperLimit), byref(self.lowerLimit), byref(self.sampleTime), byref(self.output))
+
+        self.setGains(pGain, iGain, dGain, filterCoeff, upperLimit, lowerLimit)
+
+    def setGains(self, pGain: float, iGain: float, dGain: float, filterCoeff: float, upperLimit: float, lowerLimit: float):
+        self.pGain.value = pGain
+        self.iGain.value = iGain
+        self.dGain.value = dGain
+        self.filterCoeff.value = filterCoeff
+        self.upperLimit.value = upperLimit
+        self.lowerLimit.value = lowerLimit
+
+    def setIntegratorState(self, integratorState: float):
+        self.integratorState.value = integratorState
+
+    def setFilterState(self, filterState: float):
+        self.filterState.value = filterState
+
+    def getOutput(self):
+        return self.output.value
+
+    def step(self, error: float, sampleTime: float) -> float:
+        self.error.value = error
+        self.sampleTime.value = sampleTime
+        cPID_Step(self.ptr_RT_MODEL_PIDController_T, self.error, self.pGain, self.iGain, self.dGain, self.filterCoeff,
+                  self.integratorState, self.filterState, self.upperLimit, self.lowerLimit, self.sampleTime, byref(self.output))
+        return self.output.value
