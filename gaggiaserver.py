ffbbde131308d1fd9964a46c123250b6aa2b867e
Marsunpaisti <marsunpaisti@gmail.com>
2022-09-09 23:28:25 +0300

    moar log

--- gaggiaserver.py
+++ gaggiaserver.py
@@ -20,7 +20,7 @@ DISABLE_PRINTS = config["disableprints"]
 telemetryAddress = None
 if (DATA_SEND_IP != None):
     telemetryAddress = (DATA_SEND_IP, DATA_SEND_PORT)
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(Logger=True)
 app = web.Application()
 sio.attach(app)
 gaggiaController = GaggiaController(telemetryAddress, sio, DISABLE_PRINTS)
