fd8f4a98ae98023f13afce40b77e85824eda6968
Marsunpaisti <marsunpaisti@gmail.com>
2022-09-18 14:37:45 +0300

    build frontend

--- frontend/src/components/GaggiaDataView.tsx
+++ frontend/src/components/GaggiaDataView.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext } from 'react';
 import {
   GaggiaDataContext,
   TemperatureReading,
@@ -10,6 +10,7 @@ import { LinePath } from '@visx/shape';
 import { SpinnerView } from './MainRouter';
 import { GridRows, GridColumns } from '@visx/grid';
 import { AxisLeft, AxisBottom } from '@visx/axis';
+import useMeasure from 'react-use-measure';
 
 const getX = (d: TemperatureReading) => d.timestamp;
 const getY = (d: TemperatureReading) => d.temperature;
@@ -20,12 +21,7 @@ const TemperatureChart = ({
   temperatureReadings: TemperatureReading[];
   timeHorizonMs: number;
 }) => {
-  const svgRef = useRef<SVGSVGElement | null>();
-  const [svgBounds, setSvgBounds] = useState<undefined | DOMRect>(undefined);
-
-  useEffect(() => {
-    setSvgBounds(svgRef.current?.getBoundingClientRect());
-  }, [svgRef]);
+  const [ref, bounds] = useMeasure();
 
   const latestTemperatureData =
     temperatureReadings[temperatureReadings.length - 1] ?? -1;
@@ -48,7 +44,7 @@ const TemperatureChart = ({
   );
   const xScale = scaleTime<number>({
     domain: timeDomain,
-    range: [0, svgBounds?.width ?? 0],
+    range: [0, bounds?.width ?? 0],
   });
 
   const temperatureDomain = [
@@ -57,7 +53,7 @@ const TemperatureChart = ({
   ];
   const temperatureYScale = scaleLinear<number>({
     domain: temperatureDomain,
-    range: [svgBounds?.height ?? 0, 0],
+    range: [bounds?.height ?? 0, 0],
   });
 
   console.log('XRange: ' + xScale.range());
@@ -65,16 +61,13 @@ const TemperatureChart = ({
 
   return (
     <div className="rounded overflow-hidden shadow-paper max-w-[800px] max-h-[500px] flex flex-1">
-      <svg
-        className="bg-stone-400 flex flex-col flex-1 w-full"
-        ref={(r) => (svgRef.current = r)}
-      >
-        {svgRef.current && svgBounds && (
+      <svg className="bg-stone-400 flex flex-col flex-1 w-full" ref={ref}>
+        {bounds && (
           <>
             <GridRows
               scale={temperatureYScale}
-              width={svgBounds!.width}
-              height={svgBounds!.height}
+              width={bounds!.width}
+              height={bounds!.height}
               stroke="#CACACA"
               tickValues={gridTickRowValues}
             />
