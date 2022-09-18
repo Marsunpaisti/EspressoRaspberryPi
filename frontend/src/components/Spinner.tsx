535a98bcf5f7b1bf50abab4af77f277895932b16
Marsunpaisti <marsunpaisti@gmail.com>
2022-09-18 21:12:46 +0300

    spuuun

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -5,7 +5,6 @@ export const Spinner = () => {
         aria-hidden="true"
         className="text-gray-400 animate-spin fill-blue-300"
         viewBox="0 0 100 101"
-        fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
