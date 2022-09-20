649d152c8065c94e8195f9d30a52fe840e5570c7
Marsunpaisti <marsunpaisti@gmail.com>
2022-09-20 19:26:36 +0300

    change to 27pins

--- shutdownallpins.py
+++ shutdownallpins.py
@@ -7,10 +7,7 @@ pins = [board.D1, board.D2, board.D3, board.D4,
         board.D13, board.D14, board.D15, board.D16,
         board.D17, board.D18, board.D19, board.D20,
         board.D21, board.D22, board.D23, board.D24,
-        board.D25, board.D26, board.D27, board.D28,
-        board.D29, board.D30, board.D31, board.D32,
-        board.D33, board.D34, board.D35, board.D36,
-        board.D37, board.D38, board.D39, board.D40]
+        board.D25, board.D26, board.D27]
 
 try:
     for pin in pins:
