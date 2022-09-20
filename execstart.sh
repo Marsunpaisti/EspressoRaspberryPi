4d05608e8f840a58844c724a7cf0767b432f9950
Marsunpaisti <marsunpaisti@gmail.com>
2022-09-20 19:40:02 +0300

    sh

--- execstart.sh
+++ execstart.sh
@@ -1,2 +1,2 @@
-/usr/bin/python /home/pi/PythonScripts/EspressoRaspberryPi/gaggiaserver.py
-/usr/bin/python /home/pi/PythonScripts/EspressoRaspberryPi/shutdownallpins.py
+/usr/bin/python /home/pi/PythonScripts/EspressoRaspberryPi/gaggiaserver.py > /home/pi/gaggiaserver.log 2>&1
+/usr/bin/python /home/pi/PythonScripts/EspressoRaspberryPi/shutdownallpins.py > /home/pi/gaggiaserver.log 2>&1
